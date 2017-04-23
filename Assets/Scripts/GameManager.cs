17efe313339f582b4ed625de9f202cdfba7b705e
pacificyost <pacificyost@gmail.com>
2017-04-22 17:23:48 -0700

    Add Collectible visual & enforce y = 0 for floor & min percent
    Added a temporary material for the collectible objects
    Updated the Box prefab so that the floor of the room is always at y = 0
    to help with player physics not falling through the floor
    Updated the Game Manager to have a minimum percent and for scaling down
    to stop at that percent.  This is to prevent the player from either
    always hitting a collectible or being thrown from the map as the room
    gets smaller than the player.  Some kind of trigger will need to be
    added to end the game when the time runs out.
    The collectible and player are now on layers so the collectible cannot
    be triggered by the environment.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,30 +5,49 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
     public float startTime = 10f;
+    public float minPercent = 0.2f;
     public static float currentTime = 10f;
     public static float percent = 1f;
     private static float bonusTime = 0f;
+    
 
 	// Use this for initialization
 	void Awake () {
         currentTime = startTime;
         bonusTime = 0f;
-        percent = 1f;
-	}
+        CalcPercent();
+    }
 	
 	// Update is called once per frame
 	void Update () {
         if (bonusTime <= 0)
         {
-            currentTime -= Time.deltaTime;
+            if (currentTime > 0)
+            {
+                currentTime -= Time.deltaTime;
+            }
         }
         else
         {
             currentTime += Time.deltaTime;
             bonusTime -= Time.deltaTime;
         }
-        percent = currentTime / startTime;
-	}
+        CalcPercent();
+
+    }
+
+    private void CalcPercent()
+    {
+        if (currentTime > 0)
+        {
+            percent = (currentTime + (startTime * minPercent)) / startTime;
+        }
+        else
+        {
+            percent = minPercent;
+        }
+
+    }
 
     public static void AddTime(float time)
     {
